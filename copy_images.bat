1bd088dee23672346b4c4cd662319ed4b07097a7
joniforseo <joniforseo@gmail.com>
2026-07-07 23:11:26 +0700

    hapus file

--- copy_images.bat
+++
@@ -1,9 +0,0 @@
-mkdir C:\xampp\htdocs\bikininwebsite.com\assets\images
-copy /y "C:\Users\Be quiet!\.gemini\antigravity\brain\0fe197d8-3ccd-438e-b69b-b11aaab205db\fb_mockup*.png" "C:\xampp\htdocs\bikininwebsite.com\assets\images\portfolio_fb.png"
-copy /y "C:\Users\Be quiet!\.gemini\antigravity\brain\0fe197d8-3ccd-438e-b69b-b11aaab205db\otomotif_mockup*.png" "C:\xampp\htdocs\bikininwebsite.com\assets\images\portfolio_otomotif.png"
-copy /y "C:\Users\Be quiet!\.gemini\antigravity\brain\0fe197d8-3ccd-438e-b69b-b11aaab205db\furniture_mockup*.png" "C:\xampp\htdocs\bikininwebsite.com\assets\images\portfolio_furniture.png"
-copy /y "C:\Users\Be quiet!\.gemini\antigravity\brain\0fe197d8-3ccd-438e-b69b-b11aaab205db\iot_mockup*.png" "C:\xampp\htdocs\bikininwebsite.com\assets\images\portfolio_iot.png"
-copy /y "C:\Users\Be quiet!\.gemini\antigravity\brain\0fe197d8-3ccd-438e-b69b-b11aaab205db\smart_device_mockup*.png" "C:\xampp\htdocs\bikininwebsite.com\assets\images\portfolio_smart_devices.png"
-copy /y "C:\Users\Be quiet!\.gemini\antigravity\brain\0fe197d8-3ccd-438e-b69b-b11aaab205db\handphone_mockup*.png" "C:\xampp\htdocs\bikininwebsite.com\assets\images\portfolio_handphone.png"
-copy /y "C:\Users\Be quiet!\.gemini\antigravity\brain\0fe197d8-3ccd-438e-b69b-b11aaab205db\electronic_mockup*.png" "C:\xampp\htdocs\bikininwebsite.com\assets\images\portfolio_electronic.png"
-copy /y "C:\Users\Be quiet!\.gemini\antigravity\brain\0fe197d8-3ccd-438e-b69b-b11aaab205db\smart_home_mockup*.png" "C:\xampp\htdocs\bikininwebsite.com\assets\images\portfolio_smarthome.png"
--- fetch.ps1
+++
@@ -1,8 +0,0 @@
-$html = (Invoke-WebRequest -Uri "https://www.chikuro.com/").Content
-$html = $html -replace 'href="public/', 'href="https://www.chikuro.com/public/'
-$html = $html -replace 'src="public/', 'src="https://www.chikuro.com/public/'
-$html = $html -replace 'href="assets/', 'href="https://www.chikuro.com/assets/'
-$html = $html -replace 'src="assets/', 'src="https://www.chikuro.com/assets/'
-$html = $html -replace "href='public/", "href='https://www.chikuro.com/public/"
-$html = $html -replace "src='public/", "src='https://www.chikuro.com/public/"
-Set-Content -Path "c:\xampp\htdocs\bikininwebsite.com\portfolio\fb.html" -Value $html
